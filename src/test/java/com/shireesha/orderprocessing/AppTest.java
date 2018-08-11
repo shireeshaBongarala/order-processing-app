ef7be9f97170e60ef6a7761c69d5133ff14a7267
Shireesha Bongarala <bongaralashireesha@gmail.com>
2018-08-11 18:09:44 +0530

    Initial setup

---
+++ src/test/java/com/shireesha/orderprocessing/AppTest.java
@@ -0,0 +1,11 @@
+package com.shireesha.orderprocessing;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void test() {
+    assertEquals(1, 1);
+  }
+}
